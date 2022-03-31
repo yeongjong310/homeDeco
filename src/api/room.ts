61242637079d2255257fc337dca006f491e3e1a6
yeongjong <tohero21@naver.com>
2022-03-31 18:10:31 +0900

    Modify - productList 요청에 따른 response 타입을 제네릭으로변경

--- src/api/room.ts
+++ src/api/room.ts
@@ -2,7 +2,7 @@ import requests from './core';
 import { Room } from './models/Room.type';
 
 export const get = {
-  productList: (): Promise<Room> => requests.get('test/image_product_link.json'),
+  productList: () => requests.get<Room>('test/image_product_link.json'),
 };
 
 export const post = {};
