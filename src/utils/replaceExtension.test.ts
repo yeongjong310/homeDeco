c3d4d456aee2dfd28d0139a9e73a6c9d7fa73b3a
yeongjong <tohero21@naver.com>
2022-03-31 21:11:09 +0900

    Modify - test 대단위 describe로 묶음 처리

--- src/utils/replaceExtension.test.ts
+++ src/utils/replaceExtension.test.ts
@@ -1,13 +1,15 @@
 import replaceFileExtension from './replaceExtension';
 
-test('jpeg 확장자를 webp 확장자로 변경합니다.', () => {
-  expect(replaceFileExtension('http://123123.naver.com/nklsdf.jpeg?123123', 'jpeg')).toBe(
-    'http://123123.naver.com/nklsdf.jpeg?123123',
-  );
-});
+describe('replaceFileExtension', () => {
+  test('jpeg 확장자를 webp 확장자로 변경합니다.', () => {
+    expect(replaceFileExtension('http://123123.naver.com/nklsdf.jpeg?123123', 'jpeg')).toBe(
+      'http://123123.naver.com/nklsdf.jpeg?123123',
+    );
+  });
 
-test('jpeg 등의 확장자 명이 우연치 않게 src에 포함되어 있더라도 확장자명 만을 찾아서 변경합니다.', () => {
-  expect(replaceFileExtension('http://jpeg.com/nklsdf.jpeg?123123', 'webp')).toBe(
-    'http://jpeg.com/nklsdf.webp?123123',
-  );
+  test('jpeg 등의 확장자 명이 우연치 않게 src에 포함되어 있더라도 확장자명 만을 찾아서 변경합니다.', () => {
+    expect(replaceFileExtension('http://jpeg.com/nklsdf.jpeg?123123', 'webp')).toBe(
+      'http://jpeg.com/nklsdf.webp?123123',
+    );
+  });
 });
